24daa5ce483689e4fa8a34589df569326197da3b
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-11-05 17:28:55 +1000

    babel

--- js/onload.js
+++ js/onload.js
@@ -2,7 +2,6 @@ jQuery( document ).ready(function() {
     var scenes = Array.from(document.querySelectorAll('[data-scene="scene"]'));
     scenes.map(scene => {new Parallax(scene, {
             pointerEvents: true,
-            relativeInput: true,
         });
     })
 
