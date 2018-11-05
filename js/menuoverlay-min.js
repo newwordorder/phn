9a230f1f7d44427bb8be6c332ddd0e7b9c896c16
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-11-06 08:53:49 +1000

    Added Array.from polyfill

--- js/menuoverlay-min.js
+++ js/menuoverlay-min.js
@@ -1,5 +1,17 @@
-class ShapeOverlays {
-  constructor(elm) {
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var ShapeOverlays =
+/*#__PURE__*/
+function () {
+  function ShapeOverlays(elm) {
+    _classCallCheck(this, ShapeOverlays);
+
     this.elm = elm;
     this.path = elm.querySelectorAll('path');
     this.numPoints = 4;
@@ -11,95 +23,122 @@ class ShapeOverlays {
     this.isOpened = false;
     this.isAnimating = false;
   }
-  toggle() {
-    this.isAnimating = true;
-    const range = Math.random() * Math.PI * 2;
-    for (var i = 0; i < this.numPoints; i++) {
-      const radian = (i / (this.numPoints - 1)) * Math.PI * 2;
-      this.delayPointsArray[i] = (Math.sin(radian + range) + 1) / 2 * this.delayPointsMax;
-    }
-    if (this.isOpened === false) {
-      this.open();
-    } else {
-      this.close();
-    }
-  }
-  open() {
-    this.isOpened = true;
-    this.elm.classList.add('is-opened');
-    this.timeStart = Date.now();
-    this.renderLoop();
-  }
-  close() {
-    this.isOpened = false;
-    this.elm.classList.remove('is-opened');
-    this.timeStart = Date.now();
-    this.renderLoop();
-  }
-  updatePath(time) {
-    const points = [];
-    for (var i = 0; i < this.numPoints; i++) {
-      points[i] = ease.cubicInOut(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100
-    }
 
-    let str = '';
-    str += (this.isOpened) ? `M 0 0 V ${points[0]} ` : `M 0 ${points[0]} `;
-    for (var i = 0; i < this.numPoints - 1; i++) {
-      const p = (i + 1) / (this.numPoints - 1) * 100;
-      const cp = p - (1 / (this.numPoints - 1) * 100) / 2;
-      str += `C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
-    }
-    str += (this.isOpened) ? `V 0 H 0` : `V 100 H 0`;
-    return str;
-  }
-  render() {
-    if (this.isOpened) {
-      for (var i = 0; i < this.path.length; i++) {
-        this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
+  _createClass(ShapeOverlays, [{
+    key: "toggle",
+    value: function toggle() {
+      this.isAnimating = true;
+      var range = Math.random() * Math.PI * 2;
+
+      for (var i = 0; i < this.numPoints; i++) {
+        var radian = i / (this.numPoints - 1) * Math.PI * 2;
+        this.delayPointsArray[i] = (Math.sin(radian + range) + 1) / 2 * this.delayPointsMax;
       }
-    } else {
-      for (var i = 0; i < this.path.length; i++) {
-        this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+
+      if (this.isOpened === false) {
+        this.open();
+      } else {
+        this.close();
       }
     }
-  }
-  renderLoop() {
-    this.render();
-    if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
-      requestAnimationFrame(() => {
-        this.renderLoop();
-      });
+  }, {
+    key: "open",
+    value: function open() {
+      this.isOpened = true;
+      this.elm.classList.add('is-opened');
+      this.timeStart = Date.now();
+      this.renderLoop();
     }
-    else {
-      this.isAnimating = false;
+  }, {
+    key: "close",
+    value: function close() {
+      this.isOpened = false;
+      this.elm.classList.remove('is-opened');
+      this.timeStart = Date.now();
+      this.renderLoop();
     }
-  }
-}
+  }, {
+    key: "updatePath",
+    value: function updatePath(time) {
+      var points = [];
 
-(function() {
-  const elmHamburger = document.querySelector('.hamburger');
-  const gNavItems = document.querySelectorAll('.global-menu__item');
-  const elmOverlay = document.querySelector('.shape-overlays');
-  const overlay = new ShapeOverlays(elmOverlay);
+      for (var i = 0; i < this.numPoints; i++) {
+        points[i] = ease.cubicInOut(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100;
+      }
 
-  elmHamburger.addEventListener('click', () => {
+      var str = '';
+      str += this.isOpened ? "M 0 0 V ".concat(points[0], " ") : "M 0 ".concat(points[0], " ");
 
+      for (var i = 0; i < this.numPoints - 1; i++) {
+        var p = (i + 1) / (this.numPoints - 1) * 100;
+        var cp = p - 1 / (this.numPoints - 1) * 100 / 2;
+        str += "C ".concat(cp, " ").concat(points[i], " ").concat(cp, " ").concat(points[i + 1], " ").concat(p, " ").concat(points[i + 1], " ");
+      }
+
+      str += this.isOpened ? "V 0 H 0" : "V 100 H 0";
+      return str;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (this.isOpened) {
+        for (var i = 0; i < this.path.length; i++) {
+          this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
+        }
+      } else {
+        for (var i = 0; i < this.path.length; i++) {
+          this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+        }
+      }
+    }
+  }, {
+    key: "renderLoop",
+    value: function renderLoop() {
+      var _this = this;
+
+      this.render();
+
+      if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
+        requestAnimationFrame(function () {
+          _this.renderLoop();
+        });
+      } else {
+        this.isAnimating = false;
+      }
+    }
+  }]);
+
+  return ShapeOverlays;
+}();
+
+(function () {
+  var elmHamburger = document.querySelector('.hamburger');
+  var gNavItems = document.querySelectorAll('.global-menu__item');
+  var elmOverlay = document.querySelector('.shape-overlays');
+  var overlay = new ShapeOverlays(elmOverlay);
+  elmHamburger.addEventListener('click', function () {
     elmHamburger.classList.toggle("is-active");
 
     if (overlay.isAnimating) {
       return false;
     }
+
     overlay.toggle();
+
     if (overlay.isOpened === true) {
       elmHamburger.classList.add('is-opened-navi');
+
       for (var i = 0; i < gNavItems.length; i++) {
         gNavItems[i].classList.add('is-opened');
       }
     } else {
       elmHamburger.classList.remove('is-opened-navi');
+
       for (var i = 0; i < gNavItems.length; i++) {
         gNavItems[i].classList.remove('is-opened');
       }
     }
   });
-}());
+})();
+
+//# sourceMappingURL=menuoverlay-min.js.map
