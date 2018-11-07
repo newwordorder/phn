3e250fc661d47a7303b8bf309632cd926fc09e59
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-11-08 09:17:20 +1000

    50 50 video is now modal

--- page-templates/blocks/50-50.php
+++ page-templates/blocks/50-50.php
@@ -29,13 +29,25 @@ $video = get_sub_field('video');
         </div>
 
         <?php else: ?>
-            <div class="video-cover h-100">
-                <div class="background-image-holder">
+                <div class="modal-instance">
+                <div class="video-play-icon video-play-icon--sm modal-trigger box-shadow"></div>
+                
+                <div class="modal-container">
+                <div class="modal-content bg-dark" data-width="60%">
+                    <div class="embed-container rounded">
+                    <?php echo $video; ?>
+                    </div>
+                </div>
+                <!--end of modal-content-->
+                </div>
+                <!--end of modal-container-->
+            </div>
+            <div class="background-image-holder" style="z-index:-1;">
                     <img alt="image" src="<?php echo $image['url']; ?>" />
                 </div>
-                <div class="video-play-icon"></div>
-                <iframe class="h-100" data-src="https://www.youtube.com/embed/6p45ooZOOPo?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
-            </div>
+        <!--     <div class="background-image-holder">
+                    <img alt="image" src="<?php echo $image['url']; ?>" />
+                </div> -->
         <?php endif; ?>
 
   </div>
